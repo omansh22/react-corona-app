caacd143544fd275a77adf472558ed580fe5cf2c
omansh22 <lavania.omansh@gmail.com>
2020-05-31 15:28:05 +0530

    second

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ import Cards from './Component/cards/card';
 import Charts from './Component/charts/charts';
 import Countrypic from './Component/country picker/country';
 import {Fetchdata} from './api/index'
+import {FaGithubSquare} from 'react-icons/fa';
+import './app.css';
 
 
 export default class App extends Component {
@@ -42,6 +44,13 @@ export default class App extends Component {
                 <Countrypic handlecountry={this.handlecountry}/>
                   
                 <Charts data={data} country={country} />
+                <div className='credits'>
+                <div className='a'>C-19 Global App</div>
+                <div className='x'><FaGithubSquare size='35'/></div>
+                <div><a className='b' href='https://github.com/omansh22/react-corona-app'>@Omansh22</a></div>
+                <div className='c'>Data is accessed by John Hopkins</div>
+                </div>
+                
             </div>
         );
     }
